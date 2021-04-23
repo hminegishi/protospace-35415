4ee15aa45333c9defb4424402013369e9e1ab7ca
hminegishi <buruucyu08@gmail.com>
2021-04-23 16:17:11 +0900

    ユーザーのアクセス範囲の制限を修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :set_prototype, only: [:edit, :show ,:update, :move_to_index]
   before_action :authenticate_user!, only: [:create, :update, :destroy]
-  before_action :move_to_index, except: [:index, :show]
+
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -21,14 +20,20 @@ class PrototypesController < ApplicationController
   end
 
   def show
+    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments.includes(:user)
   end
 
   def edit
+    @prototype = Prototype.find(params[:id])
+    unless @prototype.user_id == current_user.id
+      redirect_to action: :index
+    end
   end
 
   def update
+    @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
       redirect_to prototype_path(prototype.id), method: :get
     else
@@ -47,13 +52,9 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  def set_prototype
-    @prototype = Prototype.find(params[:id])
-  end
-
   def move_to_index
-    unless user_signed_in? && current_user.id == @prototype.user_id
-        redirect_to action: :index
+    unless user_signed_in? 
+      redirect_to action: :index
     end
   end
 end
